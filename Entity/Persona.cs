913ba6e6df2e40390e65885d4af9555114505578
gonzaleztomi1978-design <gonzaleztomi1978@gmail.com>
2026-06-08 22:55:14 -0300

    clase abstracta Persona con encapsulamiento

--- Entity/Persona.cs
+++ Entity/Persona.cs
@@ -1,13 +1,20 @@
+
 public abstract class Persona
 {
+    
     private string nombre;
     private string dni;
 
     public string Nombre { get => nombre; set => nombre = value; }
     public string Dni { get => dni; set => dni = value; }
-    public Persona(string nombre, string dni)
+
+   
+    protected Persona(string nombre, string dni)
     {
-        this.Nombre = nombre;
-        this.Dni = dni;
+        this.nombre = nombre;
+        this.dni = dni;
     }
+
+    
+    public abstract string MostrarInformacion();
 }
