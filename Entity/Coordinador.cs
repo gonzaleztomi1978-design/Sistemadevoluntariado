61f6f0a8a9803de27dfd097c0374632ccced0e97
gonzaleztomi1978-design <gonzaleztomi1978@gmail.com>
2026-06-08 22:58:38 -0300

    clase Coordinador hereda de Persona con areas y personas a cargo

--- Entity/Coordinador.cs
+++ Entity/Coordinador.cs
@@ -1,7 +1,21 @@
-public class Voluntario : Persona
+public class Coordinador : Persona
 {
-    public Voluntario(string nombre, string dni, int cantidadPersonas, string areaAsignada) : base(nombre, dni, cantidadPersonas, areaAsignada)
+    private List<string> areasAsignadas;
+    private int cantidadPersonasACargo;
+
+    public List<string> AreasAsignadas { get => areasAsignadas; set => areasAsignadas = value; }
+    public int CantidadPersonasACargo { get => cantidadPersonasACargo; set => cantidadPersonasACargo = value; }
+
+    public Coordinador(string nombre, string dni, List<string> areasAsignadas, int cantidadPersonasACargo)
+        : base(nombre, dni)
     {
-        
+        this.areasAsignadas = areasAsignadas;
+        this.cantidadPersonasACargo = cantidadPersonasACargo;
+    }
+
+    public override string MostrarInformacion()
+    {
+        string areas = string.Join(", ", AreasAsignadas);
+        return $"Coordinador | Nombre: {Nombre} | DNI: {Dni} | Áreas asignadas: {areas} | Personas a cargo: {CantidadPersonasACargo}";
     }
 }
