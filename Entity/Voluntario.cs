098096a993411bbd104a5a6f9a3403a42eda0731
gonzaleztomi1978-design <gonzaleztomi1978@gmail.com>
2026-06-08 22:58:31 -0300

    clase Voluntario hereda de Persona con horas trabajadas

--- Entity/Voluntario.cs
+++ Entity/Voluntario.cs
@@ -1,7 +1,15 @@
 public class Voluntario : Persona
 {
-    public Voluntario(string nombre, string dni, int cantidadTrabajadas) : base(nombre, dni, cantidadTrabajadas)
+    private int horasTrabajadas;
+    public int HorasTrabajadas { get => horasTrabajadas; set => horasTrabajadas = value; }
+
+    public Voluntario(string nombre, string dni, int horasTrabajadas) : base(nombre, dni)
     {
-        
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public override string MostrarInformacion()
+    {
+        return $"Voluntario  | Nombre: {Nombre} | DNI: {Dni} | Horas trabajadas: {HorasTrabajadas}";
     }
 }
